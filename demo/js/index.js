ac6982f85d1290dd23559b24ccd705ac6a604bb4
julesmevaa <jules.stagiaire-it@glotelhomail.com>
2021-08-17 17:56:45 +0100

    :wrench:

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,6 +1,9 @@
 class Slide {
     joinParams = {};
     el;
+    parent;
+    elWidth = 0;
+    childWidth = 0;
     elementInitLeft = 0;
     pointerStart = 0;
     pointerEnd = 0;
@@ -26,7 +29,7 @@ class Slide {
      * @return {string}
      */
     getGridTemplateValue(nbr, value) {
-        const data = typeof value === 'string' ? value : `${value}px`;
+        const data = typeof value === 'string' ? value : `${Math.round(value * 1000) / 1000}px`;
         return `repeat(${nbr}, ${data})`;
     }
     /**
@@ -40,31 +43,43 @@ class Slide {
      * @param child
      */
     adjustChildren(el, childCount, ratio, firstChildWidth, parentWidth, gap, child) {
+        this.childWidth = this.calculItemWidth(ratio, firstChildWidth, parentWidth, gap, childCount);
         el.style.display = "grid";
-        el.style.gridTemplateColumns = this.getGridTemplateValue(childCount, this.calculItemWidth(ratio, firstChildWidth, parentWidth, gap, childCount));
+        el.style.gridTemplateColumns = this.getGridTemplateValue(childCount, this.childWidth);
         el.style.gap = `${gap}px`;
         child.forEach((e) => {
             e.style.width = '100%';
         });
+        this.elWidth = (this.childWidth + gap - 1) * childCount;
     }
     onPointerMove(e) {
-        console.log(e);
-        this.el.style.transform = `translateX(${this.translateX - (this.pointerStart - e.pageX)}px)`;
+        const move = this.translateX - (this.pointerStart - e.pageX);
+        const restVisible = this.elWidth + move;
+        if (move < 0 && restVisible > this.parent.getBoundingClientRect().width) {
+            this.el.style.transform = `translateX(${move}px)`;
+            this.pointerEnd = move;
+        }
     }
     onPointerUp(e) {
         this.el.removeEventListener('pointermove', this.eventPointerMove);
-        this.pointerEnd = e.pageX;
+        this.el.style.transition = `${this.joinParams.duration}s`;
+        this.el.style.transform = `translateX(${this.replacer(this.pointerEnd)}px)`;
+    }
+    replacer(move) {
+        const ratio = Math.round(move / this.childWidth);
+        console.log(this.childWidth * ratio + (ratio * (this.joinParams.gap - 1)));
+        return Math.floor(this.childWidth * ratio + (ratio * (this.joinParams.gap - 1))) - 1;
     }
     onPointerDown(e) {
         e.preventDefault();
+        this.el.style.transition = 'none';
         const style = window.getComputedStyle(this.el);
         const matrix = new WebKitCSSMatrix(style.transform);
         this.translateX = matrix.m41;
         this.pointerStart = e.pageX;
         this.eventPointerMove = this.onPointerMove.bind(this);
         this.el.addEventListener('pointermove', this.eventPointerMove);
-        this.el.addEventListener('pointerup', this.onPointerUp.bind(this), { once: true });
-        this.el.addEventListener('pointerout', this.onPointerUp.bind(this), { once: true });
+        document.addEventListener('pointerup', this.onPointerUp.bind(this), { once: true });
     }
     /**
      * Add a slide comportment to children of element
@@ -75,19 +90,21 @@ class Slide {
         this.joinParams = {
             ...{
                 autoAdjust: false,
-                gap: 0
+                gap: 0,
+                duration: 0
             },
             ...params
         };
         this.el = el;
+        this.parent = this.el.parentElement;
+        this.el.style.transition = `${this.joinParams.duration}s`;
         // TODO if autoAdjust is true make adjust
         if (this.joinParams.autoAdjust) {
             const firstChild = this.el.firstElementChild;
-            const parent = this.el.parentElement;
             const child = Array.from(this.el.children);
             const childCount = child.length;
             const firstChildWidth = firstChild?.getBoundingClientRect().width || 0;
-            const parentWidth = parent?.getBoundingClientRect().width || 0;
+            const parentWidth = this.parent?.getBoundingClientRect().width || 0;
             const ratio = Math.trunc((parentWidth + this.joinParams.gap) / (firstChildWidth + this.joinParams.gap));
             this.adjustChildren(this.el, childCount, ratio, firstChildWidth, parentWidth, this.joinParams.gap, child);
         }
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 class Slide {
   joinParams: any = {}
   el: HTMLElement
+  parent: HTMLElement
+  elWidth = 0
+  childWidth = 0
   elementInitLeft = 0
   pointerStart: number = 0
   pointerEnd: number = 0
@@ -29,7 +32,7 @@ class Slide {
    * @return {string}
    */
   getGridTemplateValue(nbr: number, value: number | string): string {
-    const data = typeof value === 'string' ? value : `${value}px`
+    const data = typeof value === 'string' ? value : `${Math.round(value * 1000) / 1000}px`
     return `repeat(${nbr}, ${data})`
   }
 
@@ -44,35 +47,48 @@ class Slide {
    * @param child
    */
   adjustChildren(el: HTMLElement, childCount: number, ratio: number, firstChildWidth: number, parentWidth: number, gap: number, child: Element[] ) {
+    this.childWidth = this.calculItemWidth(ratio, firstChildWidth, parentWidth, gap, childCount)
     el.style.display = "grid"
-    el.style.gridTemplateColumns = this.getGridTemplateValue(childCount, this.calculItemWidth(ratio, firstChildWidth, parentWidth, gap, childCount))
+    el.style.gridTemplateColumns = this.getGridTemplateValue(childCount, this.childWidth)
     el.style.gap = `${gap}px`
 
     child.forEach((e: Element) => {
       (e as HTMLElement).style.width = '100%'
     })
+    this.elWidth = (this.childWidth + gap - 1) * childCount
   }
 
   onPointerMove(e: MouseEvent) {
-    console.log(e)
-    this.el.style.transform = `translateX(${this.translateX - (this.pointerStart - e.pageX)}px)`
+    const move = this.translateX - (this.pointerStart - e.pageX)
+    const restVisible = this.elWidth + move
+
+    if (move < 0 && restVisible > this.parent.getBoundingClientRect().width) {
+      this.el.style.transform = `translateX(${move}px)`
+      this.pointerEnd = move
+    }
   }
 
   onPointerUp(e: MouseEvent) {
     this.el.removeEventListener('pointermove', this.eventPointerMove)
-    this.pointerEnd = e.pageX
+    this.el.style.transition = `${this.joinParams.duration}s`
+    this.el.style.transform = `translateX(${this.replacer(this.pointerEnd)}px)`
+  }
+  replacer(move: number): number {
+    const ratio = Math.round(move / this.childWidth)
+    console.log(this.childWidth * ratio + (ratio * (this.joinParams.gap - 1)))
+    return Math.floor(this.childWidth * ratio + (ratio * (this.joinParams.gap - 1))) -  1
   }
 
   onPointerDown(e: MouseEvent) {
     e.preventDefault()
+    this.el.style.transition = 'none'
     const style = window.getComputedStyle(this.el);
     const matrix = new WebKitCSSMatrix(style.transform);
     this.translateX = matrix.m41
     this.pointerStart = e.pageX
     this.eventPointerMove = this.onPointerMove.bind(this)
     this.el.addEventListener('pointermove', this.eventPointerMove)
-    this.el.addEventListener('pointerup', this.onPointerUp.bind(this), { once: true })
-    this.el.addEventListener('pointerout', this.onPointerUp.bind(this), { once: true })
+    document.addEventListener('pointerup', this.onPointerUp.bind(this), { once: true })
   }
 
   /**
@@ -84,20 +100,22 @@ class Slide {
     this.joinParams = {
       ...{
         autoAdjust: false,
-        gap: 0
+        gap: 0,
+        duration: 0
       },
       ...params
     }
     this.el = el
+    this.parent = this.el.parentElement as HTMLElement
+    this.el.style.transition = `${this.joinParams.duration}s`
     // TODO if autoAdjust is true make adjust
     if (this.joinParams.autoAdjust) {
       const firstChild = this.el.firstElementChild
-      const parent = this.el.parentElement
       const child = Array.from(this.el.children)
       const childCount = child.length
 
       const firstChildWidth = firstChild?.getBoundingClientRect().width || 0
-      const parentWidth = parent?.getBoundingClientRect().width || 0
+      const parentWidth = this.parent?.getBoundingClientRect().width || 0
 
       const ratio = Math.trunc((parentWidth + this.joinParams.gap) / (firstChildWidth + this.joinParams.gap))
 
