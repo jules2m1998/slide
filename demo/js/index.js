5f0bc9b676cbf880ca227940942a38f9cd5caea4
julesmevaa <jules.stagiaire-it@glotelhomail.com>
2021-08-17 16:05:44 +0100

    :wrench:

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,60 +1,103 @@
-/**
- * Calcul la taille d'un élement ajustée à celle de son parent
- * @param ratio {number}
- * @param itemWidth {number}
- * @param parentWidth {number}
- * @param gap {number}
- * @param nbChild
- * @return {number}
- */
-function calculItemWidth(ratio, itemWidth, parentWidth, gap, nbChild) {
-    const rest = parentWidth - (ratio * itemWidth + (gap * (ratio - 1)));
-    return itemWidth + rest / ratio;
-}
-/**
- * Retourne le grid template column à appliquer à l'élement
- * @param nbr
- * @param value
- * @return {string}
- */
-function getGridTemplateValue(nbr, value) {
-    const data = typeof value === 'string' ? value : `${value}px`;
-    return `repeat(${nbr}, ${data})`;
-}
-/**
- * Add a slide comportment to children of element
- * @param el {HTMLElement} element dom when you want to make children slide
- * @param params {Object} the parameters to apply
- */
-function Slide(el, params = {}) {
-    const joinParams = {
-        ...{
-            autoAdjust: false,
-            gap: 0
-        },
-        ...params
-    };
-    // TODO if autoAdjust is true make adjust
-    if (joinParams.autoAdjust) {
-        const firstChild = el.firstElementChild;
-        const parent = el.parentElement;
-        const child = Array.from(el.children);
-        const childCount = child.length;
-        const firstChildWidth = firstChild?.getBoundingClientRect().width || 0;
-        const parentWidth = parent?.getBoundingClientRect().width || 0;
-        const ratio = Math.trunc((parentWidth + joinParams.gap) / (firstChildWidth + joinParams.gap));
-        console.log(parentWidth, firstChildWidth, ratio);
+class Slide {
+    joinParams = {};
+    el;
+    elementInitLeft = 0;
+    pointerStart = 0;
+    pointerEnd = 0;
+    eventPointerMove;
+    translateX = 0;
+    /**
+     * Calcul la taille d'un élement ajustée à celle de son parent
+     * @param ratio {number}
+     * @param itemWidth {number}
+     * @param parentWidth {number}
+     * @param gap {number}
+     * @param nbChild
+     * @return {number}
+     */
+    calculItemWidth(ratio, itemWidth, parentWidth, gap, nbChild) {
+        const rest = parentWidth - (ratio * itemWidth + (gap * (ratio - 1)));
+        return itemWidth + rest / ratio;
+    }
+    /**
+     * Retourne le grid template column à appliquer à l'élement
+     * @param nbr
+     * @param value
+     * @return {string}
+     */
+    getGridTemplateValue(nbr, value) {
+        const data = typeof value === 'string' ? value : `${value}px`;
+        return `repeat(${nbr}, ${data})`;
+    }
+    /**
+     * Ajuste la taille des enfant d'un élement
+     * @param el
+     * @param childCount
+     * @param ratio
+     * @param firstChildWidth
+     * @param parentWidth
+     * @param gap
+     * @param child
+     */
+    adjustChildren(el, childCount, ratio, firstChildWidth, parentWidth, gap, child) {
         el.style.display = "grid";
-        el.style.gridTemplateColumns = getGridTemplateValue(childCount, calculItemWidth(ratio, firstChildWidth, parentWidth, joinParams.gap, childCount));
-        el.style.gap = `${joinParams.gap}px`;
+        el.style.gridTemplateColumns = this.getGridTemplateValue(childCount, this.calculItemWidth(ratio, firstChildWidth, parentWidth, gap, childCount));
+        el.style.gap = `${gap}px`;
         child.forEach((e) => {
             e.style.width = '100%';
         });
     }
-    // TODO Add scroll behavior
-    // TODO Add finger scroll behavior
-    // TODO Apply params
-    // TODO manage responsive
+    onPointerMove(e) {
+        console.log(e);
+        this.el.style.transform = `translateX(${this.translateX - (this.pointerStart - e.pageX)}px)`;
+    }
+    onPointerUp(e) {
+        this.el.removeEventListener('pointermove', this.eventPointerMove);
+        this.pointerEnd = e.pageX;
+    }
+    onPointerDown(e) {
+        e.preventDefault();
+        const style = window.getComputedStyle(this.el);
+        const matrix = new WebKitCSSMatrix(style.transform);
+        this.translateX = matrix.m41;
+        this.pointerStart = e.pageX;
+        this.eventPointerMove = this.onPointerMove.bind(this);
+        this.el.addEventListener('pointermove', this.eventPointerMove);
+        this.el.addEventListener('pointerup', this.onPointerUp.bind(this), { once: true });
+        this.el.addEventListener('pointerout', this.onPointerUp.bind(this), { once: true });
+    }
+    /**
+     * Add a slide comportment to children of element
+     * @param el {HTMLElement} element dom when you want to make children slide
+     * @param params {Object} the parameters to apply
+     */
+    constructor(el, params = {}) {
+        this.joinParams = {
+            ...{
+                autoAdjust: false,
+                gap: 0
+            },
+            ...params
+        };
+        this.el = el;
+        // TODO if autoAdjust is true make adjust
+        if (this.joinParams.autoAdjust) {
+            const firstChild = this.el.firstElementChild;
+            const parent = this.el.parentElement;
+            const child = Array.from(this.el.children);
+            const childCount = child.length;
+            const firstChildWidth = firstChild?.getBoundingClientRect().width || 0;
+            const parentWidth = parent?.getBoundingClientRect().width || 0;
+            const ratio = Math.trunc((parentWidth + this.joinParams.gap) / (firstChildWidth + this.joinParams.gap));
+            this.adjustChildren(this.el, childCount, ratio, firstChildWidth, parentWidth, this.joinParams.gap, child);
+        }
+        // TODO Add scroll behavior
+        this.el.style.position = 'relative';
+        this.elementInitLeft = this.el.getBoundingClientRect().left;
+        this.el.addEventListener('pointerdown', this.onPointerDown.bind(this));
+        // TODO Add finger scroll behavior
+        // TODO Apply params
+        // TODO manage responsive
+    }
 }
 export default Slide;
-console.log('Hello word');
--- index.ts
+++ index.ts
@@ -1,70 +1,116 @@
-/**
- * Calcul la taille d'un élement ajustée à celle de son parent
- * @param ratio {number}
- * @param itemWidth {number}
- * @param parentWidth {number}
- * @param gap {number}
- * @param nbChild
- * @return {number}
- */
-function calculItemWidth(ratio: number, itemWidth: number, parentWidth: number, gap: number, nbChild: number): number {
-  const rest = parentWidth - (ratio * itemWidth + (gap * (ratio - 1)))
+class Slide {
+  joinParams: any = {}
+  el: HTMLElement
+  elementInitLeft = 0
+  pointerStart: number = 0
+  pointerEnd: number = 0
+  eventPointerMove: any
+  translateX = 0
 
-  return itemWidth + rest / ratio
-}
+  /**
+   * Calcul la taille d'un élement ajustée à celle de son parent
+   * @param ratio {number}
+   * @param itemWidth {number}
+   * @param parentWidth {number}
+   * @param gap {number}
+   * @param nbChild
+   * @return {number}
+   */
+  calculItemWidth(ratio: number, itemWidth: number, parentWidth: number, gap: number, nbChild: number): number {
+    const rest = parentWidth - (ratio * itemWidth + (gap * (ratio - 1)))
 
-/**
- * Retourne le grid template column à appliquer à l'élement
- * @param nbr
- * @param value
- * @return {string}
- */
-function getGridTemplateValue(nbr: number, value: number | string): string {
-  const data = typeof value === 'string' ? value : `${value}px`
-  return `repeat(${nbr}, ${data})`
-}
-
-/**
- * Add a slide comportment to children of element
- * @param el {HTMLElement} element dom when you want to make children slide
- * @param params {Object} the parameters to apply
- */
-function Slide(el: HTMLElement, params: any = {}) {
-  const joinParams = {
-    ...{
-      autoAdjust: false,
-      gap: 0
-    },
-    ...params
+    return itemWidth + rest / ratio
   }
-  // TODO if autoAdjust is true make adjust
-  if (joinParams.autoAdjust) {
-    const firstChild = el.firstElementChild
-    const parent = el.parentElement
-    const child = Array.from(el.children)
-    const childCount = child.length
 
-    const firstChildWidth = firstChild?.getBoundingClientRect().width || 0
-    const parentWidth = parent?.getBoundingClientRect().width || 0
+  /**
+   * Retourne le grid template column à appliquer à l'élement
+   * @param nbr
+   * @param value
+   * @return {string}
+   */
+  getGridTemplateValue(nbr: number, value: number | string): string {
+    const data = typeof value === 'string' ? value : `${value}px`
+    return `repeat(${nbr}, ${data})`
+  }
 
-    const ratio = Math.trunc((parentWidth + joinParams.gap) / (firstChildWidth + joinParams.gap))
-
-    console.log(parentWidth, firstChildWidth, ratio)
-
+  /**
+   * Ajuste la taille des enfant d'un élement
+   * @param el
+   * @param childCount
+   * @param ratio
+   * @param firstChildWidth
+   * @param parentWidth
+   * @param gap
+   * @param child
+   */
+  adjustChildren(el: HTMLElement, childCount: number, ratio: number, firstChildWidth: number, parentWidth: number, gap: number, child: Element[] ) {
     el.style.display = "grid"
-    el.style.gridTemplateColumns = getGridTemplateValue(childCount, calculItemWidth(ratio, firstChildWidth, parentWidth, joinParams.gap, childCount))
-    el.style.gap = `${joinParams.gap}px`
+    el.style.gridTemplateColumns = this.getGridTemplateValue(childCount, this.calculItemWidth(ratio, firstChildWidth, parentWidth, gap, childCount))
+    el.style.gap = `${gap}px`
 
     child.forEach((e: Element) => {
       (e as HTMLElement).style.width = '100%'
     })
   }
-  // TODO Add scroll behavior
-  // TODO Add finger scroll behavior
-  // TODO Apply params
-  // TODO manage responsive
+
+  onPointerMove(e: MouseEvent) {
+    console.log(e)
+    this.el.style.transform = `translateX(${this.translateX - (this.pointerStart - e.pageX)}px)`
+  }
+
+  onPointerUp(e: MouseEvent) {
+    this.el.removeEventListener('pointermove', this.eventPointerMove)
+    this.pointerEnd = e.pageX
+  }
+
+  onPointerDown(e: MouseEvent) {
+    e.preventDefault()
+    const style = window.getComputedStyle(this.el);
+    const matrix = new WebKitCSSMatrix(style.transform);
+    this.translateX = matrix.m41
+    this.pointerStart = e.pageX
+    this.eventPointerMove = this.onPointerMove.bind(this)
+    this.el.addEventListener('pointermove', this.eventPointerMove)
+    this.el.addEventListener('pointerup', this.onPointerUp.bind(this), { once: true })
+    this.el.addEventListener('pointerout', this.onPointerUp.bind(this), { once: true })
+  }
+
+  /**
+   * Add a slide comportment to children of element
+   * @param el {HTMLElement} element dom when you want to make children slide
+   * @param params {Object} the parameters to apply
+   */
+  constructor(el: HTMLElement, params: any = {}) {
+    this.joinParams = {
+      ...{
+        autoAdjust: false,
+        gap: 0
+      },
+      ...params
+    }
+    this.el = el
+    // TODO if autoAdjust is true make adjust
+    if (this.joinParams.autoAdjust) {
+      const firstChild = this.el.firstElementChild
+      const parent = this.el.parentElement
+      const child = Array.from(this.el.children)
+      const childCount = child.length
+
+      const firstChildWidth = firstChild?.getBoundingClientRect().width || 0
+      const parentWidth = parent?.getBoundingClientRect().width || 0
+
+      const ratio = Math.trunc((parentWidth + this.joinParams.gap) / (firstChildWidth + this.joinParams.gap))
+
+      this.adjustChildren(this.el, childCount, ratio, firstChildWidth, parentWidth, this.joinParams.gap, child)
+    }
+    // TODO Add scroll behavior
+    this.el.style.position = 'relative'
+    this.elementInitLeft = this.el.getBoundingClientRect().left
+    this.el.addEventListener('pointerdown', this.onPointerDown.bind(this))
+    // TODO Add finger scroll behavior
+    // TODO Apply params
+    // TODO manage responsive
+  }
 }
 
 export default Slide
-
-console.log('Hello word')
